6066a9ea51804232391f54fa1b09259ba8ffc662
j3ffjones <jeff.jones@openmailbox.org>
2014-05-09 05:57:57 +0300

    new commit

--- src/pm/entitymanager/logic/file/TestFileLogic.java
+++ src/pm/entitymanager/logic/file/TestFileLogic.java
@@ -6,7 +6,6 @@
 package pm.entitymanager.logic.file;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,15 +18,12 @@ import pm.entitymanager.logic.EntityNotFolderishException;
  *
  * @author grinias
  */
-public class TestFileLogic  {
-    
+public class TestFileLogic  {    
     private static AbstractFile fileSystemsRoot;
     private static AbstractFile currentDir;
     private static String currentPath;
     private static String fullPath;
-    private static List<String> cFullPath;
-    
-  //  private PathNameSanitization path;
+    private static List<String> cFullPath; //Storing the full path that the user is currebtly in including the root folder
     public static void initialize() throws IOException {
         fileSystemsRoot = new FileSystemsRoot();
         currentDir = fileSystemsRoot;
@@ -35,8 +31,7 @@ public class TestFileLogic  {
         fullPath = new String();
         System.out.println("CurrentPath: " + currentPath);
         System.out.println("CurrentPath alias: " + currentDir.getAlias());
-        cFullPath = new ArrayList<>();
-        System.out.println("Cln tst");
+        cFullPath = new ArrayList<>();        
     }
     private static void clearConsole() {
         try {
@@ -53,49 +48,43 @@ public class TestFileLogic  {
     }
     public static void displayDirContents(List<AbstractFile> dirContents) throws EntityNotFolderishException {
         
-        for (AbstractFile file : dirContents) {
+        dirContents.stream().forEach((file) -> {
             String fileName = file.getFullName();
             String fileType = (file.isFolderish() ? "D" : "F");
             String permissions = (file.isReadable() ? "R" : "-");
             permissions += (file.isWritable() ? "W" : "-");
             Date date = file.getLastModified();
-            
-            String fmpath = ""; //new PathNameSanitization(null).getFullPath(fullPath)
-        //    System.out.println("Full Path: " + path.getFullPath());
-            System.out.printf("\t %s\t\t\t\t %-2s %s %12.1f \t%-27s \t%s%n", fileName, fileType, permissions, file.getSize() /1024.0, date.toString() , file.getAlias());        
-        } 
+            System.out.printf("\t %s\t\t\t\t %-2s %s %12.1f \t%-27s \t%s%n", fileName, fileType, permissions, file.getSize() /1024.0, date.toString() , file.getAlias());
+        }); 
     }
     public static void displayPathAndContents() throws EntityNotFolderishException {
         clearConsole();
         System.out.println("Path:" + currentPath); 
         System.out.println("FullPath: " + fullPath);
-        System.out.println("FullPath List: " + cFullPath.toString());
-        
+        System.out.println("FullPath auto print List: " + cFullPath.toString());
+        cFullPath.stream().forEach((file) -> {
+            System.out.printf("%s", file);
+        });
+        System.out.printf("\n");
         List<AbstractFile> dirContents = currentDir.getAllChildren();   
         displayDirContents(dirContents);
     }
-    public static void moveToParent() {
-        if (currentDir.getParent() != null) {
-         
+    public static void moveToParent() {        
+        if (currentDir.getParent() != null) {         
             currentDir = currentDir.getParent();
-            System.out.println("currentDir: "+ currentDir.getName());
-            
-            Path parentPath=Paths.get(currentPath).getParent();
-
            if((Paths.get(currentPath).getParent())==null && (currentPath.equals(fileSystemsRoot.getAlias())))  {
-               System.out.println("Entered ");
-               currentPath = "";           
+               currentPath = "";
+               fullPath="";               
+               cFullPath.remove(cFullPath.size() - 1);                
            }
             System.out.println("currPath: " + currentPath);
-            if((Paths.get(currentPath).getParent())==null){ 
-                System.out.println("Entered ");
+            if((Paths.get(currentPath).getParent())==null) {
                 currentPath=currentDir.getName();
-                System.out.println("currPath: " + currentPath);
-                        }
-
-
-
+                cFullPath.remove(cFullPath.size() - 1);
+                fullPath = fullPath.replace(currentPath, "");                        
+            }
         }
+        
     }
 public static String dm(String md5) {
    try {
@@ -110,55 +99,41 @@ public static String dm(String md5) {
     }
     return null;
 }
-    public static void moveToChild(String selection, Boolean isRoot) throws EntityNotFolderishException {
-        
+    public static void moveToChild(String selection, Boolean isRoot) throws EntityNotFolderishException {        
         List<AbstractFile> children = currentDir.getAllChildren();
         AbstractFile selectedDir = null;
         System.out.println("selection: " + selection);
         if(isRoot) {
-            for (AbstractFile anEntity : children) {
-            selection= new PathNameSanitization(selection).getOnlyLetter();
-             System.out.println("selection San: " + selection);
-            if (anEntity.getCharNameOnly().equals(selection= new PathNameSanitization(selection).getOnlyLetter()) && anEntity.isFolderish()) {
-                selectedDir =  anEntity;
-                break;
-            }
             
-           //TODO: get root drive
-
+            for (AbstractFile anEntity : children) {
+                selection= new PathNameSanitization(selection).getOnlyLetter();            
+                if (anEntity.getCharNameOnly().equals(selection= new PathNameSanitization(selection).getOnlyLetter()) && anEntity.isFolderish()) {
+                    selectedDir =  anEntity;
+                    break;                    
+            }
         }
             String drive;
             drive= new PathNameSanitization(selection).getDrive();
-            fullPath=drive;
-        System.out.println("cu Path: " + selection);
-        cFullPath.add(drive);
+            fullPath=drive;        
+            cFullPath.add(drive);
     }
         if(!isRoot) { for (AbstractFile anEntity : children) {
             if (anEntity.getFullName().equals(selection= new PathNameSanitization(selection).getDirectoryName()) && anEntity.isFolderish()) {
                 selectedDir =  anEntity;
                 break;
             }
-
-            //TODO: get current folder
-     // --->       path.getFullPath(folder= new PathNameSanitization(selection).getDirectoryName());
-        }
-            
+        }            
             fullPath+=  new PathNameSanitization(selection).getDirectoryName();
-            String directory = new PathNameSanitization(selection).getDirectoryName();
+            String directory = "\\" + new PathNameSanitization(selection).getDirectoryName();            
             cFullPath.add(directory);
         }
         if(selectedDir==null) System.out.println("selected dir null");
         System.out.println("currentPath: " + currentPath);
         if (selectedDir != null) {
             currentDir = selectedDir;
-          //  currentDir
-         //   System.out.println("currentDir name: " + currentDir.getFullName().toString());
-        //    System.out.println("currentPath: " + currentPath);
             if(!(currentPath.equals(currentDir.getName())))    currentPath = Paths.get( currentDir.getFullName()).toString();
             else Paths.get(currentDir.getFullName(),currentDir.getFullName()).toString();
-         //   System.out.println("currentPath: " + currentPath);
-        }
-        
+        }        
     }
     /**
      * @param args the command line arguments
@@ -174,7 +149,7 @@ public static String dm(String md5) {
             System.out.print("Select a directory ('..' to go up one level,'.' for exit):");
             selection = consoleInput.nextLine();            
             System.out.println("Selection " + selection);
-            Boolean isRoot=true;
+            Boolean isRoot=true; // is inside a root child ?
             do {
                 if (selection.equals("..")) {
                     moveToParent();
@@ -186,14 +161,11 @@ public static String dm(String md5) {
                 }
                 displayPathAndContents();
                 System.out.print("Select a directory ('..' to go up one level,'.' for exit):");
-                selection = consoleInput.nextLine();
-                
+                selection = consoleInput.nextLine();                
             }
             while (!selection.equals("."));
         } catch (EntityNotFolderishException ex) {
             Logger.getLogger(TestFileLogic.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
-
 }
